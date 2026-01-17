8d244386d23cae2dd68d7b43889eac5ffd9fb49f
Peturbir <peturbir@gmail.com>
2026-01-17 14:17:37 +0000

    Added pygame clock and delta time calculation.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     #Initialize Pygame
     pygame.init()
 
+    #Clock time
+    clock = pygame.time.Clock()
+    dt = 0
+
     #Set up the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -14,16 +18,23 @@ def main():
 
     #Game loop
     while True:
+
         #Log state
         log_state()
+
         #Check for player input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         #Draw game
         screen.fill("black")
+
         #Refresh display
         pygame.display.flip()
+
+        #Limit framerate
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
