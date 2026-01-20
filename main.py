3f0d0f0e04279043632b850378ccc54133aa8521
Peturbir <peturbir@gmail.com>
2026-01-20 21:55:12 +0000

    Created groups for .update() and .draw() on the player object.

--- main.py
+++ main.py
@@ -17,11 +17,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    #Initialize player
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     #Game loop
     while True:
+
+        #Limit framerate
+        dt = clock.tick(60) / 1000
 
         #Log state
         log_state()
@@ -31,19 +38,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
             
         #Draw game
         screen.fill("black")
 
         #Draw player
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
 
         #Refresh display
         pygame.display.flip()
-
-        #Limit framerate
-        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
